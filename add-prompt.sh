0a8e285d0ba1155da263b4ff7ea1a5f0c842c5aa
superbuggy <jpqreichard@gmail.com>
2017-11-28 12:27:29 -0500

    more escape

--- add-prompt.sh
+++ add-prompt.sh
@@ -7,7 +7,7 @@ echo "source ~/.git-prompt.sh" >> ~/.bash_profile
 
 echo "GIT_PS1_SHOWDIRTYSTATE=1" >> ~/.bash_profile
 echo "git_prompt='\$(__git_ps1)'" >> ~/.bash_profile
-echo "PS1=\"\[\e[33m\]  \d \t \w$git_prompt\n\[\e[m\]\\$\"" >> ~/.bash_profile
+echo "PS1=\"\[\e[33m\]  \d \t \w\$git_prompt\n\[\e[m\]\\$\"" >> ~/.bash_profile
 
 source ~/.bash_profile
 echo "End!"
