a7ef65bffe249ec56aa2014c2cd02bd4e93a0401
Mike Nabil <maikel_nabil@yahoo.com>
2017-11-30 15:55:11 -0500

    fixed the prompt script

--- add-prompt.sh
+++ add-prompt.sh
@@ -1,14 +1,13 @@
+# Run this script by running:   curl -o ~/prompt https://git.generalassemb.ly/raw/DC-WDI/installfest/master/add-prompt.sh && source ~/add-prompt.sh
 
-echo "#Add prompt script from https://git.generalassemb.ly/DC-WDI/installfest/blob/master/add-prompt.sh" >> ~/.bash_profile
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-brew install bash-completion
-echo "source \$(brew --prefix)/etc/bash_completion" >> ~/.bash_profile
-echo "source ~/.git-prompt.sh" >> ~/.bash_profile
+curl -o ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
-echo "GIT_PS1_SHOWDIRTYSTATE=1" >> ~/.bash_profile
-echo "git_prompt='\$(__git_ps1)'" >> ~/.bash_profile
-echo "PS1=\"\[\e[33m\]  \d \t \w\$git_prompt\n\[\e[m\]\\$\"" >> ~/.bash_profile
+echo '#Git CLI Markup' >> ~/.bash_profile
+echo 'source ~/.git-prompt.sh' >> ~/.bash_profile
+echo 'source ~/.git-completion.bash' >> ~/.bash_profile
+echo 'GIT_PS1_SHOWDIRTYSTATE=1' >> ~/.bash_profile
+echo 'git_prompt="$(__git_ps1)"' >> ~/.bash_profile
+echo 'PS1="\[\e[33m\]  \d \t \w$git_prompt\n\[\e[m\]\$ "' >> ~/.bash_profile
 
 source ~/.bash_profile
-echo "End!"
-exit
