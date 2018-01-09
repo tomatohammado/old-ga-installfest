d1e69e8835ca9f95ce9562130ad6c4b510f84504
Mike Nabil <maikel_nabil@yahoo.com>
2018-01-09 15:36:03 -0500

    edited bash profile content

--- bash_profile.sh
+++ bash_profile.sh
@@ -8,5 +8,12 @@ GIT_PS1_SHOWDIRTYSTATE=1
 git_prompt='$(__git_ps1)'
 PS1="\[\e[33m\]  \d \t \w$git_prompt\n\[\e[m\]\$ "
 
-# Ruby Environment
+# VSCode
+export PATH=$PATH:/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin
+
+# PostgreSQL
+export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
+
+# Ruby Environment - Keep this at the end of bash_profile
+export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
